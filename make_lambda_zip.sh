79918a60092c534b5c0d329395f7b3dfbbae58c8
mark on github <markongithub@users.noreply.github.com>
2024-12-22 13:21:42 -0500

    Upgrade to Python 3.12

--- make_lambda_zip.sh
+++ make_lambda_zip.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 export TZ=UTC
 TIMESTAMP=$(date +"%Y%m%d-%H%M%S")
 OUTPUT_ZIP=${PWD}/lambda_${TIMESTAMP}.zip
 
 VENV=$(pipenv --venv)
-pushd ${VENV}/lib/python3.7/site-packages
+pushd ${VENV}/lib/python3.12/site-packages
 ls | grep -v boto | xargs zip -r9 ${OUTPUT_ZIP}
 popd
 find . -name "*.py" | xargs zip -r9 ${OUTPUT_ZIP}
